c08f89ea6e9bab4ee6c1744121173214fa65a868
botere <botereaimesome@gmail.com>
2023-12-10 14:40:21 +0000

    ok commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClientFormComponent } from './client/client-form/client-form.component';
+import { ClientListComponent } from './client/client-list/client-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClientFormComponent
+    ClientFormComponent,
+    ClientListComponent
   ],
   imports: [
     BrowserModule,
